7c964d67b7bdd29688a424372d07c2998fd2bef9
Superf1cial <overwatchanalyst@gmail.com>
2016-09-19 11:31:15 +0200

    Update Reddit_WikiBot.py

--- Reddit_WikiBot.py
+++ Reddit_WikiBot.py
@@ -4,22 +4,36 @@ import praw
 import time
 import traceback
 import obot
+import sqlite3
 
 print("Setting up Config")
 
 ''' Config '''
 
-SUBREDDIT = 'PythonWikiBotTesting'
+sql = sqlite3.connect('commentdata')
+cur = sql.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(id TEXT)')
 
-MAXPOSTS = 60
+SUBREDDIT = 'BotFinalTesting'
 
-WAIT = 30
+WAIT = 15
 
 ERROR_WAIT = 20
 
-REPLY =""
+DO_SUBMISSIONS = False
+DO_COMMENTS = True
 
-''' ALL DONE '''
+MAXPOSTS = 100
+
+CLEANCYCLES = 10
+
+REPLY ='''
+##This action was performed by a bot, please contact /u/Superf1cial for bugs
+'''
+
+already_done= []
+
+''' Config '''
 
 print('Logging into Reddit')
 
@@ -28,7 +42,7 @@ r = obot.login()
 print('Login succesful')
 
 
-def parse_data(keyword):
+def parse_data(keyword,REPLY):
     keyword = keyword.replace("Wiki!", "")
     url = ('http://overwatch.gamepedia.com/{0}'.format(keyword))
     raw = requests.get(url)
@@ -50,43 +64,58 @@ def parse_data(keyword):
         temp = temp.replace("b'","").replace("{","").replace("}","").replace("'","")
         temp = temp.replace("\\xe2\\x9c\\x95","")
         Abilities.append(temp)
-    return Abilities
+        REPLY += "      {}\n".format(temp)
+    return REPLY
 
 def scan_reddit():
 
-    already_done = []
     keywords= ['Wiki!Genji', 'Wiki!McCree', 'Wiki!Pharah', 'Wiki!Reaper', 'Wiki!Tracer', 'Wiki!Bastion', 'Wiki!Hanzo', 'Wiki!Junkrat',
                'Wiki!Mei', 'Wiki!Torbjörn', 'Wiki!Widowmaker', 'Wiki!D.Va', 'Wiki!Reinhardt', 'Wiki!Roadhog', 'Wiki!Winston', 'Wiki!Zarya',
                'Wiki!Ana', 'Wiki!Lucio', 'Wiki!Mercy', 'Wiki!Symmetra', 'Wiki!Zenyatta','Soldier:_76']
 
-    reddit = r.get_subreddit(SUBREDDIT)
-    for c in praw.helpers.comment_stream(r,reddit,limit=None):
-        has_word = any(string in c.body for string in keywords)
-        if c.id not in already_done and has_word:
-            author = c.author
-            word = next(filter(lambda x: x in c.body, keywords), None)
-            data = parse_data(word)
-            print('Replying to {}s comment'.format(author))
-            c.reply('''
-I'm just a bot, please hate my developer /u/Superf1cial.
+    print('Searching %s.' % SUBREDDIT)
+    subreddit = r.get_subreddit(SUBREDDIT)
+    posts = []
+    posts += list(subreddit.get_comments(limit=MAXPOSTS))
+    posts.sort(key=lambda x: x.created_utc)
+    print(posts)
 
-**Abilities**
+    for post in posts:
+        pid = post.id
 
-        {}
+        try:
+            pauthor = post.author.name
+        except AttributeError:
+            continue
 
-        {}
+        if pauthor.lower() == r.user.name.lower():
+            print('Will not reply to myself.')
+            continue
 
-        {}
+        cur.execute('SELECT * FROM oldposts WHERE ID=?', [pid])
+        if cur.fetchone():
+            continue
 
-        {}
+        if isinstance(post, praw.objects.Comment):
+            pbody = post.body
+        else:
+            pbody = '%s %s' % (post.title, post.selftext)
+        pbody = pbody.lower()
 
-        {}
+        if not any(key.lower() in pbody for key in keywords):
+            continue
 
-Please don't abuse me, I have feelings.
-            '''.format(data[0], data[1],data[2],data[3], data[4]))
-            already_done.append(c.id)
-            print(already_done)
+        cur.execute('INSERT INTO oldposts VALUES(?)', [pid])
+        sql.commit()
+        print('Replying to %s by %s' % (pid, pauthor))
+        word = next(filter(lambda x: x in post.body, keywords), None)
+        COMMENT = parse_data(word, REPLY)
+        try:
+                post.reply(COMMENT)
+        except praw.errors.Forbidden:
+            print('403 FORBIDDEN - is the bot banned from {}?'.format(post.subreddit.display_name))
 
+CYCLES = 0
 while True:
     try:
         scan_reddit()
@@ -94,5 +123,11 @@ while True:
         traceback.print_exc()
         print('An error occured, waiting {}'.format(ERROR_WAIT))
         time.sleep(ERROR_WAIT)
+    if CYCLES >= CLEANCYCLES:
+        print('time to clean up')
+        cur.execute('DELETE FROM oldposts WHERE id NOT IN (SELECT id FROM oldposts ORDER BY id DESC LIMIT ?)', [MAXPOSTS *2])
+        sql.commit()
+        CYCLES = 0
     print('Running again in {} seconds'.format(WAIT))
     time.sleep(WAIT)
+
